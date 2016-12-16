e3b5daacb4991fc35ea1ec7cf8101f4dc3926d4c
Stephano Hong <stephano.j.hong@gmail.com>
2016-12-16 16:31:23 -0600

    Add validations and strong params for model. Add state-updating functionality on React front-end. Add POST request functionality on React front-end. Add bootstrap basic styling on front-end.

--- app/assets/javascripts/components/contact.js.jsx
+++ app/assets/javascripts/components/contact.js.jsx
@@ -1,24 +1,64 @@
 var ContactForm = React.createClass({ 
   displayName: "ContactForm",
 
+  getInitialState: function(){
+    return {
+      firstname: null,
+      lastname: null,
+      email: null,
+      message: null,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  },
+
+  handleChange: function(field, word){
+    var change={};
+    change[field] = word.target.value;
+    this.setState(change);
+  },
+
   handleSubmit: function(event){
     event.preventDefault();
-    console.warn('submit has been triggered');
-
-    //Submit to server and render success message to user
-    
+    console.warn('submit has been triggered ' + this.state + ' ' + this.state.lastname);
+    $.post('/contact', this.state).done(function(response){
+      alert(response.message);
+    });
+    this.setState({
+      firstname: null,
+      lastname: null,
+      email: null,
+      message: null
+    });
   },
 
   render: function(){
     return (
       <div className="container">
-        <div className="col-sm-12">
+        <div className="col-md-12">
           <form onSubmit={this.handleSubmit}>
+            <div className="title form-group">
+              <h1>Add a Contact</h1>
+            </div>
             <div className="inputs">
-              <input type="text" name="contact[firstname]" placeholder="first name" />
-              <input type="text" name="contact[lastname]" placeholder="last name" />
-              <input type="text" name="contact[email]" placeholder="email" />
-              <input type="text" name="concact[message]" placeholder="message" />
+              <div className="form-group">
+                <label for="firstnameInput">First Name:</label>
+                <input type="text" className="form-control" id="firstnameInput" value={this.state.firstname} onChange={this.handleChange.bind(this, "firstname")} />
+              </div>
+              <div className="form-group">
+                <label for="lastnameInput">Last Name:</label>
+                <input type="text" className="form-control" id="lastnameInput" value={this.state.lastname} onChange={this.handleChange.bind(this, "lastname")} />
+              </div>
+              <div className="form-group">
+                <label for="emailInput">Email:</label>
+                <input type="email" className="form-control" id="emailInput" value={this.state.email} onChange={this.handleChange.bind(this, "email")} />
+              </div>
+
+              <div className="form-group">
+                <label for="messageInput">Message:</label>
+                <textarea className="form-control" id="messageInput" value={this.state.message} onChange={this.handleChange.bind(this, "message")} />
+              </div>
             </div>
             <div className="actions">
               <button className="btn btn-primary">Submit</button>
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,3 @@
 class Contact < ActiveRecord::Base
-	
+	validates :firstname, :lastname, :email, :message, presence: true
 end
